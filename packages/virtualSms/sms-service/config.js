9526394f8b864fe425c1517398ec7e53b3ee63d5
vito <vitotu@qq.com>
2023-08-30 22:46:37 +0800

    update config files

--- packages/virtualSms/sms-service/config.js
+++ packages/virtualSms/sms-service/config.js
@@ -7,7 +7,7 @@ exports.host = {
 }
 
 exports.config = {
-  "proxy":"http://192.168.106.12:10809",
+  "proxy":"http://192.168.168.95:10809",
   "mainPath":"https://sms24.me/en/numbers",
   "numberUrlPrefix":"https://sms24.me/en/numbers/"
 }

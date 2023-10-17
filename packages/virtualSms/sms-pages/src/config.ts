111783c6390796bbfb483b7290df57f26b9de7e5
vito <vitotu@qq.com>
2023-10-18 00:43:02 +0800

    fix sms config

--- packages/virtualSms/sms-pages/src/config.ts
+++ packages/virtualSms/sms-pages/src/config.ts
@@ -1,6 +1,18 @@
+import fs from 'fs'
+
+let config:any = {}
+
+try {
+  config = JSON.parse(fs.readFileSync('../../config.json', 'utf-8'))
+} catch (error) {
+  console.log(error)
+}
+
+const devConfig:any = config?.local || {}
+
 export const HOST_CONFIG = {
   api: 'http://localhost',
-  apiPort: 3080,
+  apiPort: devConfig.port || 3080,
   getApiPrefix () {
     let url ='http://' +  window.location.host.split(':')[0] + ':' + this.apiPort
     return url

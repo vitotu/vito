c7d7205e37cfc6ae01f905fb77c1f80004400355
vito <vitotu@qq.com>
2023-10-27 23:59:59 +0800

    udpate file-pages

--- packages/file-server/file-pages/src/components/SideMenuContent.vue
+++ packages/file-server/file-pages/src/components/SideMenuContent.vue
@@ -15,16 +15,26 @@ const subFolders = computed(() => {
 </script>
 
 <template>
-  <div>
+  <div class="side-menu-content">
     <div
       v-for="(node, index) in subFolders"
       :key="node.name + index"
+      class="menu-item"
     >
       {{ node.name }}
     </div>
+    <div class="menu-preview">上一级</div>
   </div>
 </template>
 
-<style>
-
+<style lang="less" scoped>
+.side-menu-content {
+  overflow-y: auto;
+  color: black;
+  height: 100%;
+  width: 100%;
+  .menu-item {
+    text-overflow: ellipsis;
+  }
+}
 </style>

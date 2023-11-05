d4085bff1097bd4aa8c2302ccf5a76f8b9e6ebb7
vito <vitotu@qq.com>
2023-11-05 22:33:14 +0800

    update file-pages

--- packages/file-server/file-pages/src/components/ImageContainer.vue
+++ packages/file-server/file-pages/src/components/ImageContainer.vue
@@ -1,11 +1,18 @@
 <script setup>
 import {} from 'vue'
 
+const props = defineProps({
+  node: {
+    type: Object,
+    required: true,
+  }
+})
+
 </script>
 
 <template>
   <div class="image-container">
-    <img src="" alt="">
+    <img :src="node.fullPath" alt="">
   </div>
 </template>
 
